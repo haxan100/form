f45c02ede2c6353be13c4eb9a99f8ab6721d9c25
haxan100 <abdul.hasan388@gmail.com>
2021-02-01 15:30:55 +0700

    templating sementara untuk user

---
+++ application/views/User/USerForm1.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>RegistrationForm_v10 by Colorlib</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- LINEARICONS -->
+	<link rel="stylesheet" href="<?= base_url()?>/templates/user/fonts/linearicons/style.css">
+
+	<!-- STYLE CSS -->
+	<link rel="stylesheet" href="<?= base_url()?>/templates/user/css/style.css">
+</head>
+
+<body>
+
+	<div class="wrapper">
+		<div class="inner">
+			<img  src="<?= base_url()?>/templates/user/images/image-1.png" alt="" class="image-1">
+			<form action="">
+				<h3>New Account?</h3>
+				<div class="form-holder">
+					<span class="lnr lnr-user"></span>
+					<input type="text" class="form-control" placeholder="Username">
+				</div>
+				<div class="form-holder">
+					<span class="lnr lnr-phone-handset"></span>
+					<input type="text" class="form-control" placeholder="Phone Number">
+				</div>
+				<div class="form-holder">
+					<span class="lnr lnr-envelope"></span>
+					<input type="text" class="form-control" placeholder="Mail">
+				</div>
+				<div class="form-holder">
+					<span class="lnr lnr-lock"></span>
+					<input type="password" class="form-control" placeholder="Password">
+				</div>
+				<div class="form-holder">
+					<span class="lnr lnr-lock"></span>
+					<input type="password" class="form-control" placeholder="Confirm Password">
+				</div>
+				<button>
+					<span>Register</span>
+				</button>
+			</form>
+			<img  src="<?= base_url()?>/templates/user/images/image-2.png" alt="" class="image-2">
+		</div>
+
+	</div>
+
+	<script  src="<?= base_url()?>/templates/user/js/jquery-3.3.1.min.js"></script>
+	<script  src="<?= base_url()?>/templates/user/js/main.js"></script>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+
+</html>
